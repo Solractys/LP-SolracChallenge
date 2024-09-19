724abe9c52cd3046cecb82b089db9fb8a553422b
Solractys <135873875+Solractys@users.noreply.github.com>
2024-09-19 20:18:26 -0300

    Update App.tsx

--- LP-1/GravityTeam/src/App.tsx
+++ LP-1/GravityTeam/src/App.tsx
@@ -20,6 +20,23 @@ export default function App() {
           </div>
         </nav>
       </header>
+      <main>
+        <div className="flex justify-center">
+          <div className="flex justify-center space-y-5 items-center flex-col w-[580px] p-10">
+            <h1 className="text-5xl antialiased font-bold text-center">
+              Balancing <br />
+              Crypto Markets
+            </h1>
+            <p className="text-center text-lg text">
+              Our fully automated proprietary quantitative trading software
+              provides 24/7 liquidity to 170+ crypto assets across 25+
+              centralized spot and derivative crypto exchanges.
+            </p>
+            <button className="bg-gradient-to-bl from-[#665DCD] via-[#5FA4E6] to-[#D2AB67] rounded-md p-2 hover:font-semibold transition-all">GET IN TOUCH</button>
+          </div>
+        </div>
+      </main>
+
     </>
   );
 }
